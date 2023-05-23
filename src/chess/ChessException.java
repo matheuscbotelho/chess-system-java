674e9c35f8b6d53bc1fce734251fe9bca5249712
Matheus Botelho <matheus.cirqueira17@gmail.com>
2023-05-23 20:17:36 -0300

    Handling exceptions and clearing screen

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,19 +1,13 @@
 package chess;
 
-<<<<<<< HEAD
 import boardgame.BoardException;
 
 public class ChessException extends BoardException {
-=======
-public class ChessException extends RuntimeException {
->>>>>>> 5995aa2a8d09db4dd1ec908260fb89cf8f9bd8ae
 	private static final long serialVersionUID = 1L;
 
 	public ChessException(String msg) {
 		super(msg);
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 5995aa2a8d09db4dd1ec908260fb89cf8f9bd8ae
+
+
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,7 +25,6 @@ public class ChessMatch {
 		return mat;
 	}
 
-<<<<<<< HEAD
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
@@ -47,31 +46,22 @@ public class ChessMatch {
 		return capturedPiece;
 	}
 
-=======
->>>>>>> 5995aa2a8d09db4dd1ec908260fb89cf8f9bd8ae
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
 
 	private void initialSetup() {
-<<<<<<< HEAD
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
 		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
 		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
 		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('d', 1, new King(board, Color.WHITE));
-
 		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
 		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
 		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
 		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('d', 8, new King(board, Color.BLACK));
-=======
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.BLACK));
->>>>>>> 5995aa2a8d09db4dd1ec908260fb89cf8f9bd8ae
 	}
 }
